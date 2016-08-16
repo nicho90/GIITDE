3a57aff68e0295cb5d9b6c254fa2e1e9cf071459
Nicho <nicho90@web.de>
2016-08-16 12:15:58 +0200

    Added example-entires

---
+++ sql/03_examples.sql
@@ -0,0 +1,33 @@
+-- Trash bins
+INSERT INTO Trash_Bins (description, filling_height, capacity, coordinates) VALUES (
+    'GEO1',
+    60,
+    60,
+    'POINT(7.595934 51.969148)'
+), (
+    'Bushaltestelle',
+    60,
+    60,
+    'POINT(7.596760 51.969357)'
+), (
+    'Correnstraße',
+    60,
+    60,
+    'POINT(7.596855 51.971076)'
+);
+
+
+-- Measurements
+INSERT INTO Measurements(trash_bin_id, measured_distance, measured_filling_height) VALUES (
+    1,
+    20,
+    40
+), (
+    2,
+    40,
+    20
+), (
+    3,
+    60,
+    0
+);
