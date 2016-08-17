a423a0575c6723908ed47776058104fa83e4600f
Nicho <nicho90@web.de>
2016-08-17 12:39:55 +0200

    Implemented GET /api/trash_bins/:trash_bin_id/measurements
    to list all measurements of a Trash bin, ordered by newest measurement

---
+++ controllers/measurements/list.js
@@ -0,0 +1,69 @@
+var colors = require('colors');
+var pg = require('pg');
+var types = require('pg').types;
+types.setTypeParser(1700, 'text', parseFloat);
+var _ = require('underscore');
+var database_url = require('../../server.js').database_url;
+
+
+// POST
+exports.request = function(req, res) {
+
+    // Connect to database
+    pg.connect(database_url, function(err, client, done) {
+        if (err) {
+            done();
+            console.error(err);
+        } else {
+
+            // Prepare Query
+            var query = "SELECT * FROM Trash_Bins WHERE trash_bin_id=$1;";
+
+            // Database query
+            client.query(query, [
+                req.params.trash_bin_id
+            ], function(err, result) {
+                done();
+
+                if (err) {
+                    res.status(500).send(err);
+                    console.error(colors.red(err));
+                } else {
+
+                    // Check if Trash_Bin exists
+                    if (result.rows.length === 0) {
+                        res.status(404).send('Trash bin not found!');
+                        console.error(colors.red('Trash bin not found!'));
+                    } else {
+
+                        // Prepare Query
+                        var query = "SELECT " +
+                            "measurement_id, " +
+                            "created AS timestamp, " +
+                            "measured_distance, " +
+                            "'CENTIMETER' AS measured_distance_unit, " +
+                            "measured_filling_height, " +
+                            "'CENTIMETER' AS measured_filling_height_unit " +
+                            "FROM Measurements WHERE trash_bin_id=$1 ORDER BY created DESC;"; /* LIMIT 1000*/
+
+                        // Database query
+                        client.query(query, [
+                            req.params.trash_bin_id,
+                        ], function(err, result) {
+                            done();
+
+                            if (err) {
+                                res.status(500).send(err);
+                                console.error(colors.red(err));
+                            } else {
+
+                                // Send result
+                                res.status(200).send(result.rows);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    });
+};
